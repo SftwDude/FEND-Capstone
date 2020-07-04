9eb704a560acd90c349ed2a15207fb911cf96eb6
unknown <SftwDude@outlook.com>
2020-07-04 14:17:29 -0400

    Add css minification

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/styles.scss'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+//export to our 'Client' library which is created by the output in webpack.dev
+export {
+    checkForName,
+    handleSubmit
+}
